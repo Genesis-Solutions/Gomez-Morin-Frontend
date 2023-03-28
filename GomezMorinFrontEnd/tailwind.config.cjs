7ceddd237ab847a126cf5a7ca00cdfe5a0070e16
MandukoGR <gurom84@outlook.com>
2023-03-27 18:01:03 -0600

    add initial setup for frontend

---
+++ GomezMorinFrontEnd/tailwind.config.cjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  darkMode: "class",
+  theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px'
+    },
+    fontFamily: {
+
+    },
+    extend: {
+
+
+    },
+  },
+  plugins: [],
+}
