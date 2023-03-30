775377d86597f9da4e00945591cb56f9932ac7fc
Miguel Weiping Tang Feng <88734115+Mike5801@users.noreply.github.com>
2023-03-30 00:08:46 -0600

    add initial setup for react redux

--- GomezMorinFrontEnd/src/App.jsx
+++ GomezMorinFrontEnd/src/App.jsx
@@ -1,12 +1,50 @@
-
+import { useDispatch, useSelector } from "react-redux"
+import { setDarkMode, setText, setUsers } from "./states/generalSlice"
 
 function App() {
-  
+  const dark = useSelector((state) => state.general.dark)
+  const text = useSelector((state) => state.general.text)
+  const users = useSelector((state) => state.general.users)
+
+  const dispatch = useDispatch()
+
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="w-full h-full">
+      <h1>DarkMode: { dark ? "dark" : "light" }</h1>
+      <h1>Text: { text }</h1>
+      <h1>
+      { users.map((user) => (
+        `${user}, `
+      )) }
+      </h1>
+      <button
+        className="p-2 border-2 border-black"
+        onClick={() => {
+          dispatch(setDarkMode())
+        }}
+      >
+        DarkMode
+      </button>
+
+      <button
+        className="p-2 border-2 border-black"
+        onClick={() => {
+          dispatch(setText("Hola Mundo"))
+        }}
+      >
+        Text
+      </button>
+
+      <button
+        className="p-2 border-2 border-black"
+        onClick={() => {
+          dispatch(setUsers(["Miguel", "Flavio"]))
+        }}
+      >
+        Users
+      </button>
+    </div>
   )
 }
 
---
+++ GomezMorinFrontEnd/src/states/generalSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  users: ["Miguel"],
+  text: "Default" ,
+  dark: true
+}
+
+export const generalSlice = createSlice({
+  name: "general",
+  initialState,
+  reducers: {
+    setUsers:  (state, action) => {
+      state.users = action.payload
+    },
+    setDarkMode: (state) => {
+      state.dark = !state.dark
+    },
+    setText: (state, action) => {
+      state.text = action.payload
+    }
+  }
+})
+
+export const { setUsers, setDarkMode, setText } = generalSlice.actions
+
+export default generalSlice.reducer
+
