ba90d39f2d4f55139605e4b4417bb8c8b5ef3e3e
Miguel Weiping Tang Feng <88734115+Mike5801@users.noreply.github.com>
2023-04-21 13:05:36 -0600

    add code formatter

--- GomezMorinFrontEnd/src/components/ImageCarousel.jsx
+++ GomezMorinFrontEnd/src/components/ImageCarousel.jsx
@@ -5,15 +5,14 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 /**
  * A component for displaying a carousel of images.
- * 
+ *
  * @param {string} title - The title of the carousel.
  * @param {string[]} images - An array of image URLs to display in the carousel.
  * @param {number} height - The height of the carousel in rem units.
- * 
+ *
  * @returns {JSX.Element} - The ImageCarousel component.
  */
 const ImageCarousel = ({ title, images, height }) => {
-
   return (
     <div className="w-full h-full relative flex justify-center items-center">
       <Carousel
@@ -24,7 +23,7 @@ const ImageCarousel = ({ title, images, height }) => {
         className="w-full h-full"
       >
         {images.map((image, index) => (
-          <div style={{ width: "100%", height: `${height}rem`}}>
+          <div style={{ width: "100%", height: `${height}rem` }}>
             <img
               key={index}
               className="brightness-50 object-cover object-center w-full h-full"
