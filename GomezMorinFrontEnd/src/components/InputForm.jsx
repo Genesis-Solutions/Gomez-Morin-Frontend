c0413cd0cbb3bba26442bf25d03b2a6617649c99
CristianA01702752 <a01702752@itesm.mx>
2023-04-21 16:25:17 -0500

    add hotfix to input form

--- GomezMorinFrontEnd/src/components/InputForm.jsx
+++ GomezMorinFrontEnd/src/components/InputForm.jsx
@@ -21,7 +21,7 @@ const InputForm = ({ label, name, type, placeholder, defaultValue }) => {
       <input
         {...register(name)}
         defaultValue={defaultValue}
-        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5 dark:bg-gray-700 dark:border-gray-600"
         type={type}
         id={name}
         placeholder={placeholder}
