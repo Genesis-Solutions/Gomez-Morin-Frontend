67127e0958e509f03cda0bdf76a693846ce3f011
AriannFernando <A01703556@itesm.mx>
2023-04-21 10:24:04 -0600

    add fix to Button component for navigation

--- GomezMorinFrontEnd/src/components/Button.jsx
+++ GomezMorinFrontEnd/src/components/Button.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 /**
  * This is a component for a button.
@@ -6,9 +7,9 @@ import React from "react";
  * @param {string} type - The type of the button.
  * @param {string} colorBg - The color of the background of the button. Example: bg-red-600
  * @param {string} colorHoverBg- The color of the background of the button when hover. Example: hover:bg-red-700
- * @param {onclick action} onClick - The action the onclick button will do.
+ * @param {string} navigation- The path that will navigate to.
  */
-const Button = ({ text, type, colorBg, colorHoverBg, onClick }) => {
+const Button = ({ text, type, colorBg, colorHoverBg, navigation}) => {
   const styles = [
     "w-full",
     colorBg,
@@ -19,8 +20,14 @@ const Button = ({ text, type, colorBg, colorHoverBg, onClick }) => {
     colorHoverBg,
   ];
 
+  const navigate = useNavigate();
+
+  const currentLink = navigation === window.location.pathname;
+
   return (
-    <button className={styles.join(" ")} type={type} onClick={onClick}>
+    <button className={styles.join(" ")} type={type} onClick={() => {
+      navigate(navigation);
+    }}>
       {text}
     </button>
   );
