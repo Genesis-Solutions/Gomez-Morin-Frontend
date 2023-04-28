0a8e5a0349a0c78ca159343e1edad800b2d2d66b
srzno <sorzanosantiago@gmail.com>
2023-04-28 16:59:48 -0600

    modify 1.2-query

--- GomezMorinFrontEnd/src/queries/queryAuth.js
+++ GomezMorinFrontEnd/src/queries/queryAuth.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const baseUrl =
-    import.meta.env.VITE_BASE_URL;
+const baseUrl = import.meta.env.VITE_BASE_URL;
 
 /**
  * Send a POST request to create a new user.
@@ -12,36 +11,41 @@ const baseUrl =
  * @param {string} data.mailRegister - The email address for the new user.
  * @param {string} data.passwordRegister - The password for the new user.
  * @returns {Promise} A promise that resolves with the response data on success or rejects with an error on failure.
+ * @param {Object} data - An object containing the user's login credentials.
+ * @param {string} data.userName - The user's username.
+ * @param {string} data.password - The user's password.
+ * @returns {Promise} A promise that resolves with the response data from the API endpoint.
+ * @throws {Error} If the data parameter is not an object or is missing required fields.
  */
-export const postUser = async(data) => {
-    const { isMoralRegister, userRegister, mailRegister, passwordRegister } =
+export const postUser = async (data) => {
+  const { isMoralRegister, userRegister, mailRegister, passwordRegister } =
     data;
-    const body = {
-        userName: userRegister,
-        email: mailRegister,
-        password: passwordRegister,
-    };
-    if (isMoralRegister) {
-        body.ptrRol = "644af272bac1cb80504dc379";
-    }
-    try {
-        const response = await axios.post(`${baseUrl}/users/`, body);
-        return response.data;
-    } catch (err) {
-        return Promise.reject(err);
-    }
+  const body = {
+    userName: userRegister,
+    email: mailRegister,
+    password: passwordRegister,
+  };
+  if (isMoralRegister) {
+    body.ptrRol = "644af272bac1cb80504dc379";
+  }
+  try {
+    const response = await axios.post(`${baseUrl}/users/`, body);
+    return response.data;
+  } catch (err) {
+    return Promise.reject(err);
+  }
 };
 
-export const loginUser = async(data) => {
-    const { user, password } = data;
-    const body = {
-        email,
-        password,
-    };
-    try {
-        const response = await axios.post(`${baseUrl}/auth/login`, body);
-        return response.data;
-    } catch (err) {
-        return Promise.reject(err);
-    }
+export const loginUser = async (data) => {
+  const { userName, password } = data;
+  const body = {
+    userName,
+    password,
+  };
+  try {
+    const response = await axios.post(`${baseUrl}/login`, body);
+    return response.data;
+  } catch (err) {
+    return Promise.reject(err);
+  }
 };
