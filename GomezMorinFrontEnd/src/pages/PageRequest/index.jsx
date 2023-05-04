1b9416391d46629da1a6575f502ca7149c5948ff
srzno <sorzanosantiago@gmail.com>
2023-05-03 18:54:54 -0600

    add page HU 2.5

--- GomezMorinFrontEnd/src/pages/PageRequest/index.jsx
+++ GomezMorinFrontEnd/src/pages/PageRequest/index.jsx
@@ -2,17 +2,25 @@ import React from "react";
 import Header from "../../components/Header";
 import Button from "../../components/Button";
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import DataGridComponent from "../../components/DataGridComponent";
 
 const PageRequest = () => {
   const navigate = useNavigate();
   const rol = localStorage.getItem("nameRol");
+  const rows = [
+    { id: 1, col1: "06096", col2: "World", col3: "Aceptado" },
+  ];
+  const columns = [
+    { field: "col1", headerName: "Folio", width: 200 },
+    { field: "col2", headerName: "Evento", width: 500 },
+    { field: "col3", headerName: "Estatus", width: 200 },
+  ];
   return (
     <div className="grid grid-cols-6 w-screen h-screen">
       <div className="h-full col-span-1 bg-sideLeftBG bg-cover bg-center bg-no-repeat"></div>
-      <div className="col-span-4 h-full flex justify-between pt-16 ">
+      <div className="col-span-4 h-full flex justify-between">
         <div className="flex flex-col items-center py-10 w-full h-full p-12 ">
-          <Header tittle={"Mis solicitudes"} />
+          <Header tittle={"Solicitudes"} />
           <div className="sm:flex py-12 w-96 h-36 p-12 sm:px-6 ">
             <Button
               text={"Nueva Solicitud"}
@@ -27,6 +35,12 @@ const PageRequest = () => {
                 }
               }}
             ></Button>
+          </div>
+          <div className="w-full">
+            <DataGridComponent
+              rows={rows}
+              columns={columns}
+            ></DataGridComponent>
           </div>
         </div>
       </div>
