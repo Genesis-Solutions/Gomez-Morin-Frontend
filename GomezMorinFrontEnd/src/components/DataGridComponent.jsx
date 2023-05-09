c305c2ee9886d4c47ac8d006836d41c9d5c5753d
MandukoGR <gurom84@outlook.com>
2023-05-08 18:50:28 -0600

    add queries to requestAll

--- GomezMorinFrontEnd/src/components/DataGridComponent.jsx
+++ GomezMorinFrontEnd/src/components/DataGridComponent.jsx
@@ -25,7 +25,7 @@ import { DataGrid } from "@mui/x-data-grid";
  */
 const DataGridComponent = ({ rows, columns }) => {
   return (
-    <div style={{ height: 300, width: "100%" }}>
+    <div style={{ height: 500, width: "100%" }}>
       <DataGrid rows={rows} columns={columns} />
     </div>
   );
