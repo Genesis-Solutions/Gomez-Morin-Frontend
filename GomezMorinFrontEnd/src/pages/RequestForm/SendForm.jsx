68f3fe5576ca81950445b088ac74f1b6d11c4284
MandukoGR <gurom84@outlook.com>
2023-05-23 12:13:44 -0600

    add loading icon to lineamientos

--- GomezMorinFrontEnd/src/pages/RequestForm/SendForm.jsx
+++ GomezMorinFrontEnd/src/pages/RequestForm/SendForm.jsx
@@ -3,6 +3,7 @@ import Button from "../../components/Button";
 import { useDispatch } from "react-redux";
 import { setFormState, showSpecificForm } from "../../states/formSlice";
 import guideLinesPdf from "../../assets/Lineamientos de uso y disfrute.pdf";
+import { ClipLoader } from "react-spinners";
 
 /**
  * Component for sending the form.
@@ -12,11 +13,31 @@ import guideLinesPdf from "../../assets/Lineamientos de uso y disfrute.pdf";
 const SendForm = () => {
   const dispatch = useDispatch();
   const [isChecked, setIsChecked] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  /**
+   * The handlePdfLoad function handles the loading of the pdf.
+   *
+   * @returns {void}
+   */
+  const handlePdfLoad = () => {
+    setIsLoading(false);
+  };
 
   return (
     <>
       <div className="flex flex-col w-full h-screen gap-5 items-center">
-        <iframe src={guideLinesPdf} width="85%" height={"100%"}></iframe>
+        <iframe
+          src={guideLinesPdf}
+          onLoad={handlePdfLoad}
+          width="85%"
+          height={"100%"}
+        ></iframe>
+        {isLoading && (
+          <div className="text-center">
+            <ClipLoader size={35} color="#000000" loading={isLoading} />
+          </div>
+        )}
         <div className="flex gap-3 self-start">
           <input
             type="checkbox"
