7e0aaae42fe2fd740979333bfc6a475c7090752c
Andrés Magaña Pérez <a01067963@itesm.mx>
2023-05-23 15:46:29 -0600

    add component for input file labels

---
+++ GomezMorinFrontEnd/src/components/InputFileLabel.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const InputFileLabel = ({}) => {
+    return (
+      <div className="w-full h-full">
+        <p className="text-sm md:text-md md:mx-auto">Ingresar documentos no mayor a 5 Mb.</p>
+      </div>
+    );
+  };
+  
+  export default InputFileLabel;
+  
