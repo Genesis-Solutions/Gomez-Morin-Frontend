f1c7b0aa69965beb40d79b22edcfc34a3ae04652
MandukoGR <gurom84@outlook.com>
2023-05-26 10:05:08 -0600

    add variables

--- GomezMorinFrontEnd/src/queries/queryAuth.js
+++ GomezMorinFrontEnd/src/queries/queryAuth.js
@@ -2,9 +2,11 @@ import axios from "axios";
 import { useDispatch } from "react-redux";
 import jwt_decode from "jwt-decode";
 import { setAccessToken, setUser } from "../states/authSlice";
-const baseUrl =
-    import.meta.env.VITE_BASE_URL;
+import viteConfig from "../../vite.config";
+const baseUrl = import.meta.env.VITE_BASE_URL;
 
+const fisico = import.meta.env.VITE_PTR_FISICO;
+const moral = import.meta.env.VITE_PTR_MORAL;
 /**
  * Send a POST request to create a new user.
  *
@@ -16,30 +18,27 @@ const baseUrl =
  * @returns {Promise} A promise that resolves with the response data on success or rejects with an error on failure.
  * @param {Object} data - An object containing the user's login credentials.
  */
-export const postUser = async(data) => {
-    const { isMoralRegister, userRegister, mailRegister, passwordRegister } =
+export const postUser = async (data) => {
+  const { isMoralRegister, userRegister, mailRegister, passwordRegister } =
     data;
-    const body = {
-        userName: userRegister,
-        email: mailRegister,
-        password: passwordRegister,
-    };
-    if (isMoralRegister) {
-        //The ones above are for production.
-        //body.ptrRol = "646f9d298506765ede040d16";
-        body.ptrRol = "6451a03436c62efad459f3b4";
-    } else {
-        //body.ptrRol = "646fa0f9c2fda4000ff84c7f";
-        body.ptrRol = "6451f22a0e8703af7a4a87b7";
-    }
+  const body = {
+    userName: userRegister,
+    email: mailRegister,
+    password: passwordRegister,
+  };
+  if (isMoralRegister) {
+    body.ptrRol = fisico;
+  } else {
+    body.ptrRol = moral;
+  }
 
-    try {
-        const response = await axios.post(`${baseUrl}/users/`, body);
-        return response.data;
-    } catch (err) {
-        console.log(response)
-        return Promise.reject(err);
-    }
+  try {
+    const response = await axios.post(`${baseUrl}/users/`, body);
+    return response.data;
+  } catch (err) {
+    console.log(response);
+    return Promise.reject(err);
+  }
 };
 
 /**
@@ -51,27 +50,27 @@ export const postUser = async(data) => {
  * @throws {Error} If the data parameter is not an object or is missing required fields.
  */
 export const useLoginUser = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const login = async(data) => {
-        const { userName, password } = data;
-        const body = {
-            userName: userName,
-            password: password,
-        };
-        try {
-            const response = await axios.post(`${baseUrl}/users/login`, body, {
-                withCredentials: true,
-            });
-            dispatch(setAccessToken(response.data.accessToken));
-            const decodedToken = jwt_decode(response.data.accessToken);
-            dispatch(setUser(decodedToken));
-            return response.data;
-        } catch (err) {
-            return Promise.reject(err.response.data.message);
-        }
+  const login = async (data) => {
+    const { userName, password } = data;
+    const body = {
+      userName: userName,
+      password: password,
     };
-    return login;
+    try {
+      const response = await axios.post(`${baseUrl}/users/login`, body, {
+        withCredentials: true,
+      });
+      dispatch(setAccessToken(response.data.accessToken));
+      const decodedToken = jwt_decode(response.data.accessToken);
+      dispatch(setUser(decodedToken));
+      return response.data;
+    } catch (err) {
+      return Promise.reject(err.response.data.message);
+    }
+  };
+  return login;
 };
 
 /**
@@ -80,37 +79,37 @@ export const useLoginUser = () => {
  * @throws {Error} If an error occurs during the logout process.
  * @returns {Promise<object>} A promise that resolves with the data returned by the server.
  */
-export const logoutUser = async() => {
-    try {
-        const response = await axios.get(`${baseUrl}/users/logout`, {
-            withCredentials: true,
-        });
-        return response.data;
-    } catch (err) {
-        return Promise.reject(err);
-    }
+export const logoutUser = async () => {
+  try {
+    const response = await axios.get(`${baseUrl}/users/logout`, {
+      withCredentials: true,
+    });
+    return response.data;
+  } catch (err) {
+    return Promise.reject(err);
+  }
 };
 
 /**
  * Sends a GET request to refresh the user's access token.
- * 
+ *
  * @throws {Error} If an error occurs during the refresh process.
  * @returns  {Promise<object>} A promise that resolves with the data returned by the server.
  */
 export const useRefreshToken = () => {
-    const dispatch = useDispatch();
-    const refresh = async() => {
-        try {
-            const response = await axios.get(`${baseUrl}/users/refreshToken`, {
-                withCredentials: true,
-            });
-            dispatch(setAccessToken(response.data.accessToken));
-            const decodedToken = jwt_decode(response.data.accessToken);
-            dispatch(setUser(decodedToken));
-            return response.data;
-        } catch (err) {
-            return Promise.reject(err);
-        }
-    };
-    return refresh;
+  const dispatch = useDispatch();
+  const refresh = async () => {
+    try {
+      const response = await axios.get(`${baseUrl}/users/refreshToken`, {
+        withCredentials: true,
+      });
+      dispatch(setAccessToken(response.data.accessToken));
+      const decodedToken = jwt_decode(response.data.accessToken);
+      dispatch(setUser(decodedToken));
+      return response.data;
+    } catch (err) {
+      return Promise.reject(err);
+    }
+  };
+  return refresh;
 };
