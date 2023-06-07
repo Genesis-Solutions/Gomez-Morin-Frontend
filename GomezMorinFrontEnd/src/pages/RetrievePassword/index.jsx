028dac68d8f746d4874ac9d613fe2fa35f5e8b13
Flavio Ruvalcaba Leija <71139354+FlavioRr@users.noreply.github.com>
2023-06-07 12:33:14 -0600

    add query and page

--- GomezMorinFrontEnd/src/pages/RetrievePassword/index.jsx
+++ GomezMorinFrontEnd/src/pages/RetrievePassword/index.jsx
@@ -2,23 +2,104 @@ import React, { useState } from "react";
 import InputForm from "../../components/InputForm";
 import Button from "../../components/Button";
 import IconTitle from "../../components/IconTitle";
-import TextButton from "../../components/TextButton";
 import hexagono from "../../../public/images/hexagono.png";
 import { useForm, FormProvider } from "react-hook-form";
-import { useLoginUser } from "../../queries/queryAuth";
 import { useNavigate } from "react-router-dom";
+import { postEmailPassword } from "../../queries/queryApi";
 
 const RetrievePass = () => {
   const methods = useForm();
+  const navigate = useNavigate();
+
+
+  const onSubmit = async (data) => {
+    try {
+      const emailData = {
+        title: "Recuperar Contraseña",
+        message: `<!DOCTYPE html>
+<html>
+<head>
+  <title>Recuperación de Contraseña</title>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+    }
+    
+    .container {
+      max-width: 500px;
+      margin: 0 auto;
+      padding: 20px;
+      background-color: #f7f7f7;
+      border: 1px solid #ccc;
+      border-radius: 5px;
+    }
+    
+    h1 {
+      color: #333;
+    }
+    
+    p {
+      margin-bottom: 10px;
+    }
+    
+    .button {
+      display: inline-block;
+      padding: 10px 20px;
+      background-color: #4CAF50;
+      color: #fff;
+      text-decoration: none;
+      border-radius: 5px;
+    }
+    
+    .logo {
+      text-align: center;
+      margin-bottom: 20px;
+    }
+    
+    .logo img {
+      max-width: 150px;
+    }
+  </style>
+</head>
+<body>
+  <div class="container">
+    
+    <h1>Recuperación de Contraseña</h1>
+    <p>Estimado/a,</p>
+    
+    <p>Hemos recibido una solicitud para recuperar la contraseña de tu cuenta. Si no realizaste esta solicitud, por favor ignora este correo.</p>
+    
+    <p>Ingresa a nuestra página de eventos Goméz Morín:</p>
+    <p>
+      <a class="button" href="http://eventos-ceceq.com" target="_blank">Cambiar Contraseña</a>
+    </p>
+    
+    
+    <p>Si tienes alguna pregunta o necesitas más información, no dudes en contactarnos. Estamos aquí para ayudarte.</p>
+    
+    <p>¡Gracias y que tengas un buen día!</p>
+    
+    <p>Tu contraseña es: </p>`,
+        email: data.email,
+        username: data.username,
+      };
+      await postEmailPassword(emailData);
+      alert("Correo enviado");
+      navigate("/login");
+    } catch (err) {
+      alert(err.response.data.message);
+    }
+  };
 
   return (
-      <div className="w-full grid grid-cols-1 lg:grid-cols-2 md:grid-cols-1 ">
+    <div className="w-full grid grid-cols-1 lg:grid-cols-2 md:grid-cols-1 ">
       <div className=" h-full  mx-14 mt-4">
         <div className=" flex lg:justify-start md:justify-center sm:justify-start lg:ml-32 ml-32 md:ml-5 mt-8">
           <IconTitle image={hexagono} headerText="Recuperar Contraseña" />
         </div>
         <FormProvider {...methods}>
           <form
+            onSubmit={methods.handleSubmit((data) => onSubmit(data))}
             className="w-full max-w-md mx-auto"
           >
             <div className="mt-2">
@@ -56,7 +137,6 @@ const RetrievePass = () => {
       </div>
       <div className="lg:flex-1 lg:bg-login lg:bg-cover lg:bg-center lg:bg-no-repeat lg:h-screen"></div>
     </div>
-    
   );
 };
 
--- GomezMorinFrontEnd/src/queries/queryApi.js
+++ GomezMorinFrontEnd/src/queries/queryApi.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const baseUrl = import.meta.env.VITE_BASE_URL;
+const baseUrl =
+    import.meta.env.VITE_BASE_URL;
 
 /**
  * Sends an email notification.
@@ -8,20 +9,37 @@ const baseUrl = import.meta.env.VITE_BASE_URL;
  * @returns {Promise<Object>} - A promise that resolves to the response object.
  * @throws {Error} - If an error occurs during the API call.
  */
-export const postEmail = async (data) => {
-  const { title, userId, message } = data;
-  const user = await axios.get(`${baseUrl}/users/${userId}`);
-  const email = user.data.email;
+export const postEmail = async(data) => {
+    const { title, userId, message } = data;
+    const user = await axios.get(`${baseUrl}/users/${userId}`);
+    const email = user.data.email;
 
-  const body = {
-    title: title,
-    textBody: message,
-    recipientEmail: email,
-  };
-  try {
-    const response = await axios.post(`${baseUrl}/email`, body);
-    return response;
-  } catch (err) {
-    return Promise.reject(err);
-  }
+    const body = {
+        title: title,
+        textBody: message,
+        recipientEmail: email,
+    };
+    try {
+        const response = await axios.post(`${baseUrl}/email`, body);
+        return response;
+    } catch (err) {
+        return Promise.reject(err);
+    }
+};
+
+export const postEmailPassword = async(data) => {
+    const { title, email, message, username } = data;
+
+    const body = {
+        title: title,
+        textBody: message,
+        recipientEmail: email,
+        userName: username,
+    };
+    try {
+        const response = await axios.post(`${baseUrl}/getPassword`, body);
+        return response;
+    } catch (err) {
+        return Promise.reject(err);
+    }
 };
