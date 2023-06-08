a8a14a745b55799ce0588e466ec729d0d1ac9729
AlejandroRuizZ <a01611451@itesm.mx>
2023-06-08 12:34:44 -0600

    add prettier to make it pretty. :)

--- GomezMorinFrontEnd/src/queries/queryApi.js
+++ GomezMorinFrontEnd/src/queries/queryApi.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-const baseUrl =
-    import.meta.env.VITE_BASE_URL;
+const baseUrl = import.meta.env.VITE_BASE_URL;
 
 /**
  * Sends an email notification.
@@ -9,22 +8,22 @@ const baseUrl =
  * @returns {Promise<Object>} - A promise that resolves to the response object.
  * @throws {Error} - If an error occurs during the API call.
  */
-export const postEmail = async(data) => {
-    const { title, userId, message } = data;
-    const user = await axios.get(`${baseUrl}/users/${userId}`);
-    const email = user.data.email;
+export const postEmail = async (data) => {
+  const { title, userId, message } = data;
+  const user = await axios.get(`${baseUrl}/users/${userId}`);
+  const email = user.data.email;
 
-    const body = {
-        title: title,
-        textBody: message,
-        recipientEmail: email,
-    };
-    try {
-        const response = await axios.post(`${baseUrl}/email`, body);
-        return response;
-    } catch (err) {
-        return Promise.reject(err);
-    }
+  const body = {
+    title: title,
+    textBody: message,
+    recipientEmail: email,
+  };
+  try {
+    const response = await axios.post(`${baseUrl}/email`, body);
+    return response;
+  } catch (err) {
+    return Promise.reject(err);
+  }
 };
 
 /**
@@ -39,19 +38,19 @@ export const postEmail = async(data) => {
  * @throws {Error} If an error occurs during the POST request.
  */
 
-export const postEmailPassword = async(data) => {
-    const { title, email, message, username } = data;
+export const postEmailPassword = async (data) => {
+  const { title, email, message, username } = data;
 
-    const body = {
-        title: title,
-        textBody: message,
-        recipientEmail: email,
-        userName: username,
-    };
-    try {
-        const response = await axios.post(`${baseUrl}/getPassword`, body);
-        return response;
-    } catch (err) {
-        return Promise.reject(err);
-    }
+  const body = {
+    title: title,
+    textBody: message,
+    recipientEmail: email,
+    userName: username,
+  };
+  try {
+    const response = await axios.post(`${baseUrl}/getPassword`, body);
+    return response;
+  } catch (err) {
+    return Promise.reject(err);
+  }
 };
